a3782143586d88850589960abad7a0ec5b1c2d09
Nemanja Mehovic <nemanjamehovic@gmail.com>
2021-06-03 00:28:31 +0200

    igra 21 backend zavrsena

---
+++ Razvoj/backend/Games/21.php
@@ -0,0 +1,177 @@
+<?php
+    //Radio Nemanja Mehovic 2018/0452
+    define("allCards",[1,2,3,4,5,6,7,8,9,10,11]);
+    class Game21
+    {
+        private $cardsAvailable;
+        private $playerCards;
+        private $dealerCards;
+        private $winner;
+
+        function __construct($currentState)
+        {
+            $this->winner = 0;
+            $this->playerCards = array();
+            $this->dealerCards = array();
+            $this->cardsAvailable = array();
+
+            foreach (allCards as $key => $value)
+                $this->cardsAvailable[$key] = $value;
+
+            if($currentState != null)
+            {
+                $this->playerCards = $currentState[0];
+                $this->dealerCards = $currentState[1];
+                foreach ($this->playerCards as $tmp)
+                    if(($pos = array_search($tmp, $this->cardsAvailable, true)) !== false)
+                        array_splice($this->cardsAvailable, $pos, 1);
+                foreach ($this->dealerCards as $tmp)
+                    if(($pos = array_search($tmp, $this->cardsAvailable, true)) !== false)
+                        array_splice($this->cardsAvailable, $pos, 1);
+            }
+            else
+            {
+                for($i = 0; $i < 4; $i++)
+                {
+                    if($i < 2)
+                        $this->playerCards[$i] = $this->generateVal();
+                    else
+                        $this->dealerCards[$i - 2] = $this->generateVal();
+                }
+
+                if($this->checkIf21($this->playerCards))
+                    $this->winner = 1;
+                else if($this->checkIf21($this->dealerCards))
+                    $this->winner = -1;
+            }
+        }
+
+        function hit()
+        {
+            if($this->winner != 0)
+                return;
+            array_push($this->playerCards,$this->generateVal());
+            if($this->checkIf21($this->playerCards))
+            {
+                $this->winner = 1;
+                return;
+            }
+            if($this->checkIfBusted($this->playerCards))
+            {
+                $this->winner = -1;
+                return;
+            }
+
+            $sum = 0;
+            foreach ($this->dealerCards as $tmp)
+                $sum += $tmp;
+            if($sum >= 17)
+                return;
+
+            array_push($this->dealerCards,$this->generateVal());
+            if($this->checkIf21($this->dealerCards))
+            {
+                $this->winner = -1;
+                return;
+            }
+            if($this->checkIfBusted($this->dealerCards))
+            {
+                $this->winner = 1;
+                return;
+            }
+        }
+
+        function stand()
+        {
+            if($this->winner != 0)
+                return;
+
+            $sumP = 0;
+            foreach ($this->playerCards as $tmp)
+                $sumP += $tmp;
+            $sumD = 0;
+            foreach ($this->dealerCards as $tmp)
+                $sumD += $tmp;
+            
+            while($sumD < 17)
+            {
+                $val = $this->generateVal();
+                $sumD += $val;
+                array_push($this->dealerCards,$val);
+            }
+            if($this->checkIfBusted($this->dealerCards))
+            {
+                $this->winner = 1;
+                return;
+            }
+            do
+            {
+                if($sumP > $sumD)
+                {
+                    $this->winner = 1;
+                    return;
+                }
+                else if($sumP < $sumD)
+                {
+                    $this->winner = -1;
+                    return;
+                }
+                array_push($this->dealerCards,$this->generateVal());
+                if($this->checkIfBusted($this->dealerCards))
+                {
+                    $this->winner = 1;
+                    return;
+                }
+            }while(true);
+        }
+
+        function getReward($bet)
+        {
+            $reward = 0;
+            if($this->winner == 1)
+                $reward = $bet * 2;
+            return $reward;
+        }
+
+        function getState()
+        {
+            return array($this->playerCards, $this->dealerCards);
+        }
+
+        function getWinner()
+        {
+            return $this->winner;
+        }
+
+        private function checkIf21($arr)
+        {
+            $sum = 0;
+            foreach ($arr as $tmp)
+                $sum += $tmp;
+            return $sum == 21;
+        }
+
+        private function checkIfBusted($arr)
+        {
+            $sum = 0;
+            foreach($arr as $tmp)
+                $sum += $tmp;
+            return $sum > 21;
+        }
+
+        private function generateVal()
+        {
+            $val = -1;
+            $pos = false;
+            do
+            {
+                $val = random_int(min($this->cardsAvailable), max($this->cardsAvailable));
+                if(($pos = array_search($val, $this->cardsAvailable, true)) !== false)
+                    array_splice($this->cardsAvailable, $pos, 1);
+            }while($pos === false);
+            return $val;
+        }
+    }
+
+
+?>
---
+++ Razvoj/backend/REST/get21.php
@@ -0,0 +1,113 @@
+<?php
+    //Radio Nemanja Mehovic 2018/0452
+    require_once "../validations.php";
+    require_once "../Games/21.php";
+
+    header("Access-Control-Allow-Origin:*");
+    header("Access-Control-Allow-Methods:POST");
+    header("Content-Type:application/json");
+    header("Access-Control-Allow-Headers:Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With");
+
+    if($_SERVER["REQUEST_METHOD"] != "POST")
+    {
+        http_response_code(400);
+        exit();
+    }
+
+    @auth();
+
+    if(!isset($_REQUEST["operation"]))
+    {
+        http_response_code(400);
+        exit();
+    }
+
+    $flag1 = session_start();
+    $flag2 = session_regenerate_id();
+    if(!$flag1 || !$flag2)
+    {
+        http_response_code(500);
+        exit();
+    }
+    try
+    {
+        $game = null;
+        switch($_REQUEST["operation"])
+        {
+            case "start":
+                if(!isset($_REQUEST["bet"]) || !checkIfInt($_REQUEST["bet"]) || $_REQUEST["bet"] <= 0)
+                {
+                    http_response_code(400);
+                    exit();
+                }
+
+                if(isset($_SESSION["started"]))
+                {
+                    session_destroy();
+                    session_start();
+                    $_SESSION = array();
+                }
+
+                @betAmmountInRange($_REQUEST["bet"]);
+                @updateBogdin($UserID, -$_REQUEST["bet"]);
+
+                $_SESSION["started"] = true;
+                $game = new Game21(null);
+                $_SESSION["bet"] = $_REQUEST["bet"];
+            break;
+            case "hit":
+                if(!isset($_SESSION["started"]))
+                {
+                    http_response_code(400);
+                    exit();
+                }
+                $game = new Game21($_SESSION["state"]);
+                $game->hit();
+            break;
+            case "stand":
+                if(!isset($_SESSION["started"]))
+                {
+                    http_response_code(400);
+                    exit();
+                }
+                $game = new Game21($_SESSION["state"]);
+                $game->stand();
+            break;
+            case "keepalive":
+                if(isset($_SESSION["started"]))
+                {
+                    if(!isset($_SESSION["keepalive"]))
+                        $_SESSION["keepalive"] = 0;
+                    else
+                        $_SESSION["keepalive"]++;
+                }
+                exit();
+            break;
+            case "end":
+                if(isset($_SESSION["started"]))
+                {
+                    $_SESSION = array();
+                    session_destroy();
+                }
+                exit();
+            break;
+            default:
+                http_response_code(400);
+                exit();
+            break;
+        }
+        $_SESSION["state"] = $game->getState();
+        $bet = $_SESSION["bet"];
+        if($game->getWinner() != 0)
+        {
+            session_destroy();
+            @updateBogdin($UserID, $game->getReward($bet));
+        }
+        echo json_encode(array($game->getWinner(),$game->getState(),$game->getReward($bet)));
+    }
+    catch(Exception $e)
+    {
+        http_response_code(500);
+        exit();
+    }
+?>

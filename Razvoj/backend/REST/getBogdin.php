b18c7a3c0740278c28c5e7b22e475bf35a791cbd
Nemanja Mehovic <nemanjamehovic@gmail.com>
2021-06-03 12:29:58 +0200

    getBogdin dodato

---
+++ Razvoj/backend/REST/getBogdin.php
@@ -0,0 +1,20 @@
+<?php
+    //Radio Nemanja Mehovic 2018/0452
+    require_once "../validations.php";
+    require_once "phpFunctions.php";
+
+    header("Access-Control-Allow-Origin:*");
+    header("Access-Control-Allow-Methods:GET");
+    header("Content-Type:application/json");
+    header("Access-Control-Allow-Headers:Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With");
+
+    if($_SERVER["REQUEST_METHOD"] != "GET")
+    {
+        http_response_code(400);
+        exit();
+    }
+
+    @auth();
+
+    echo @getBogdin($UserID);
+?>

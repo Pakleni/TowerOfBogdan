0003400e67c965446598f30b4784d986f0af82eb
pakleni <ognjenbjel@protonmail.com>
2021-06-05 19:20:51 +0200

    added vip frontend

--- Razvoj/frontend/pages/buy/vip.js
+++ Razvoj/frontend/pages/buy/vip.js
@@ -1,10 +1,10 @@
 // Autor: Ognjen Bjeletic 2018/0447
-import React from "react";
+import React, { useState } from "react";
 import Title from "../../components/title";
 import NotLogged from "../../components/notlogged";
 import Head from "next/head";
 
-function Buy(stripe, level) {
+function Buy(stripe, level, errorSetter) {
   const username = sessionStorage.getItem("email");
   const pass = sessionStorage.getItem("password");
 
@@ -19,6 +19,8 @@ function Buy(stripe, level) {
   })
     .then(function (response) {
       if (response.status === 200) return response.json();
+      if (response.status === 202)
+        return Promise.reject(new Error("Already >= VIP level"));
       else return Promise.reject(new Error("Request rejected!"));
     })
     .then(function (session) {
@@ -30,11 +32,14 @@ function Buy(stripe, level) {
       }
     })
     .catch(function (error) {
-      console.error("Error:", error);
+      if (error.message === "Already >= VIP level") errorSetter(true);
+      else console.error(error);
     });
 }
 
 export default function VipStatus() {
+  const [VIPError, setVIPError] = useState(false);
+
   let isLogged = false;
 
   const ISSERVER = typeof window === "undefined";
@@ -62,7 +67,7 @@ export default function VipStatus() {
             className="button title is-3-5 is-info"
             onClick={() =>
               // eslint-disable-next-line no-undef
-              Buy(Stripe(process.env.stripe), 1)
+              Buy(Stripe(process.env.stripe), 2, setVIPError)
             }
           >{`VIP Level 1`}</button>
         </div>
@@ -71,7 +76,7 @@ export default function VipStatus() {
             className="button title is-3-5 is-success"
             onClick={() =>
               // eslint-disable-next-line no-undef
-              Buy(Stripe(process.env.stripe), 2)
+              Buy(Stripe(process.env.stripe), 3, setVIPError)
             }
           >{`VIP Level 2`}</button>
         </div>
@@ -80,10 +85,18 @@ export default function VipStatus() {
             className="button title is-3-5 is-danger"
             onClick={() =>
               // eslint-disable-next-line no-undef
-              Buy(Stripe(process.env.stripe), 3)
+              Buy(Stripe(process.env.stripe), 4, setVIPError)
             }
           >{`VIP Level 3`}</button>
         </div>
+        {VIPError === true && (
+          <div>
+            <br />
+            <div className="notification is-danger is-light">
+              <p className="title is-6">Already at equal or higher VIP level</p>
+            </div>
+          </div>
+        )}
       </div>
       <div className="section">
         <p className="subtitle is-6 has-text-centered-mobile">

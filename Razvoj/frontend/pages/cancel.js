0fae45aa4ccb2df650fd7ad8dde61883eacb7ba1
pakleni <ognjenbjel@protonmail.com>
2021-06-05 18:03:50 +0200

    add vip buy option

---
+++ Razvoj/frontend/pages/cancel.js
@@ -0,0 +1,27 @@
+// Autor: Ognjen Bjeletic 2018/0447
+import React from "react";
+import Title from "../components/title";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Title title="Home"></Title>
+      <div className="section">
+        <div className="tile is-ancestor">
+          <div className="tile is-child is-4"></div>
+          <div className="tile is-child is-4">
+            <div className="notification is-danger is-light">
+              <p className="title is-4">Purchase cancelled :(</p>
+              <p className="title is-4">
+                <Link href="/account">
+                  <a>{"<- back"}</a>
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
